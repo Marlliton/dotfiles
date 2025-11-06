c2ac3031fbf83fd20b76eb1df71134e965b124f8
Marlliton Souza <marlliton.souza1@gmail.com>
2025-11-06 18:33:09 -0300

    chore: adiciona instruções

--- nvim/.config/nvim/lua/plugins/coc-angular.lua
+++ nvim/.config/nvim/lua/plugins/coc-angular.lua
@@ -1,3 +1,5 @@
+-- INFO: Precisa configurar o lsp do angular globalmente, aversão do node importa pois
+-- uso o asdf. Veja o arquvio coc-settings.json para entender os caminhos dos binários.
 return {
   "neoclide/coc.nvim",
 }
