a8d4ab092f0f3dbe339ae375dbc03c0e2f1d68ab
Marlliton Souza <marlliton.souza1@gmail.com>
2026-01-06 19:41:10 -0300

    refactor: altera keys mini

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,12 +1,20 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = true,
+        },
+      },
+    },
     keymap = {
       preset = "default",
       ["<C-k>"] = { "select_prev", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
       ["<Tab>"] = { "accept", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
+      -- ["<CR>"] = { "accept", "fallback" },
     },
   },
 }
