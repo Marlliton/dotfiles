592f2c8971018a8eef6a3c56a946ab6899d56a0f
Marlliton Souza <marlliton.souza1@gmail.com>
2026-01-08 16:46:05 -0300

    fix: tmux-copy

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,39 +1,40 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 
-  # private envs (if file "envs" exists)
-  set envs_file (status dirname)/envs
-  if test -f $envs_file
-    source $envs_file
-  end
+    # private envs (if file "envs" exists)
+    set envs_file (status dirname)/envs
+    if test -f $envs_file
+        source $envs_file
+    end
 
-  # load ASDF and completions
-  source ~/.asdf/asdf.fish
-  source ~/.asdf/completions/asdf.fish
+    # load ASDF and completions
+    source ~/.asdf/asdf.fish
+    source ~/.asdf/completions/asdf.fish
 
-  alias update-nvim-stable='asdf uninstall neovim stable; and asdf install neovim stable'
-  alias update-nvim-nightly='asdf uninstall neovim nightly; and asdf install neovim nightly'
-  alias update-nvim-master='asdf uninstall neovim ref:master; and asdf install neovim ref:master'
+    alias update-nvim-stable='asdf uninstall neovim stable; and asdf install neovim stable'
+    alias update-nvim-nightly='asdf uninstall neovim nightly; and asdf install neovim nightly'
+    alias update-nvim-master='asdf uninstall neovim ref:master; and asdf install neovim ref:master'
 
-  # CARGO
-  set -gx PATH "$HOME/.cargo/bin" $PATH
-  
-  # RUST ALTERNATIVES
-  alias ls="exa --icons"
-  alias ll="exa --icons -l"
-  alias la="exa --icons -la"
-  alias cat="bat --style=auto"
+    # CARGO
+    set -gx PATH "$HOME/.cargo/bin" $PATH
+    set -gx PATH "$HOME/.local/bin" $PATH
 
-  # GO (Golang) configuration
-  if test -f ~/.asdf/plugins/golang/set-env.fish
-      source ~/.asdf/plugins/golang/set-env.fish
-  end
+    # RUST ALTERNATIVES
+    alias ls="exa --icons"
+    alias ll="exa --icons -l"
+    alias la="exa --icons -la"
+    alias cat="bat --style=auto"
 
-  # Add Go binaries to PATH
-  # set golang_version (asdf where golang)
-  # set -gx PATH "$(asdf where golang)/bin" $PATH
-  
-  starship init fish | source
+    # GO (Golang) configuration
+    if test -f ~/.asdf/plugins/golang/set-env.fish
+        source ~/.asdf/plugins/golang/set-env.fish
+    end
 
-  zoxide init fish | source
+    # Add Go binaries to PATH
+    # set golang_version (asdf where golang)
+    # set -gx PATH "$(asdf where golang)/bin" $PATH
+
+    starship init fish | source
+    fzf --fish | source
+    zoxide init fish | source
 end
